0df6de0780f8f4d7588afaf3531a19a594daabdb
4413 <saichandanreddyk@vt.edu>
2023-04-20 14:05:10 -0400

    Main_changes

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import numpy as np
 import pandas as pd
-from recommender import recommender,string_to_list
-import json
-import bs4 as bs
-import urllib.request
 import pickle
-import requests
+from recommender import recommender,string_to_list
+from review_scraping import reviews
 from flask import Flask, render_template, request
-import matplotlib.pyplot as plt
+
 
 data=pd.read_csv('main_data.csv')
 model_pickel = 'naive_bayes.pkl'
@@ -69,28 +66,9 @@ def recommend():
     
     # combining multiple lists as a dictionary which can be passed to the html file so that it can be processed easily and the order of information will be preserved
     movie_cards = {rec_posters[i]: rec_movies[i] for i in range(len(rec_posters))}
-    
     casts = {cast_names[i]:[cast_ids[i], cast_chars[i],cast_profiles[i]] for i in range(len(cast_profiles))}
-
-    # web scraping to get user reviews from IMDB site
-    sauce = urllib.request.urlopen('https://www.imdb.com/title/{}/reviews?ref_=tt_ov_rt'.format(imdb_id)).read()
-    soup = bs.BeautifulSoup(sauce,'lxml')
-    soup_result = soup.find_all("div",{"class":"text show-more__control"})
-
-    reviews_list = [] # list of reviews
-    reviews_status = [] # list of comments (good or bad)
-    for reviews in soup_result:
-        if reviews.string:
-            reviews_list.append(reviews.string)
-            # passing the review to our model
-            movie_review_list = np.array([reviews.string])
-            movie_vector = vectorizer.transform(movie_review_list)
-            pred = model.predict(movie_vector)
-            reviews_status.append('Good' if pred else 'Bad')
-
-    # combining reviews and comments into a dictionary
-    movie_reviews = {reviews_list[i]: reviews_status[i] for i in range(len(reviews_list))}     
-
+    
+    movie_reviews=reviews(model,vectorizer,imdb_id)
     # passing all the data to the html file
     return render_template('recommend.html',title=title,poster=poster,overview=overview,vote_average=vote_average,
         vote_count=vote_count,release_date=release_date,runtime=runtime,status=status,genres=genres,
