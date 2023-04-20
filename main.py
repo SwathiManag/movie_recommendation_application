17f150c988271c58f8286c9acc63d516648e4931
4413 <saichandanreddyk@vt.edu>
2023-04-20 10:45:36 -0400

    cast details

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import numpy as np
 import pandas as pd
 from recommender import recommender,string_to_list
+import json
 import bs4 as bs
 import urllib.request
 import pickle
+import requests
 from flask import Flask, render_template, request
+import matplotlib.pyplot as plt
 
 data=pd.read_csv('main_data.csv')
 model_pickel = 'naive_bayes.pkl'
@@ -16,17 +19,18 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html',suggestions=list(data['movie_title'].str.capitalize()))
+    suggestions = list(data['movie_title'].str.capitalize())
+    return render_template('home.html',suggestions=suggestions)
 
 @app.route("/similarity",methods=["POST"])
 def similarity():
     movie = request.form['name']
-    similar_movies,score= recommender(movie)
-    if type(similar_movies)==type('string'):
-        return similar_movies
+    rc,score= recommender(movie)
+    if type(rc)==type('string'):
+        return rc
     else:
-        movies="---".join(similar_movies)
-        return movies
+        m_str="---".join(rc)
+        return m_str
 
 @app.route("/recommend",methods=["POST"])
 def recommend():
@@ -35,9 +39,6 @@ def recommend():
     cast_ids = request.form['cast_ids']
     cast_names = request.form['cast_names']
     cast_chars = request.form['cast_chars']
-    cast_bdays = request.form['cast_bdays']
-    cast_bios = request.form['cast_bios']
-    cast_places = request.form['cast_places']
     cast_profiles = request.form['cast_profiles']
     imdb_id = request.form['imdb_id']
     poster = request.form['poster']
@@ -59,26 +60,17 @@ def recommend():
     rec_posters = string_to_list(rec_posters)
     cast_names = string_to_list(cast_names)
     cast_chars = string_to_list(cast_chars)
-    cast_profiles = string_to_list(cast_profiles)
-    cast_bdays = string_to_list(cast_bdays)
-    cast_bios = string_to_list(cast_bios)
-    cast_places = string_to_list(cast_places)
+    cast_profiles = string_to_list(cast_profiles)\
     
     # convert string to list (eg. "[1,2,3]" to [1,2,3])
     cast_ids = cast_ids.split(',')
     cast_ids[0] = cast_ids[0].replace("[","")
     cast_ids[-1] = cast_ids[-1].replace("]","")
     
-    # rendering the string to python string
-    for i in range(len(cast_bios)):
-        cast_bios[i] = cast_bios[i].replace(r'\n', '\n').replace(r'\"','\"')
-    
     # combining multiple lists as a dictionary which can be passed to the html file so that it can be processed easily and the order of information will be preserved
     movie_cards = {rec_posters[i]: rec_movies[i] for i in range(len(rec_posters))}
     
-    casts = {cast_names[i]:[cast_ids[i], cast_chars[i], cast_profiles[i]] for i in range(len(cast_profiles))}
-
-    cast_details = {cast_names[i]:[cast_ids[i], cast_profiles[i], cast_bdays[i], cast_places[i], cast_bios[i]] for i in range(len(cast_places))}
+    casts = {cast_names[i]:[cast_ids[i], cast_chars[i],cast_profiles[i]] for i in range(len(cast_profiles))}
 
     # web scraping to get user reviews from IMDB site
     sauce = urllib.request.urlopen('https://www.imdb.com/title/{}/reviews?ref_=tt_ov_rt'.format(imdb_id)).read()
@@ -102,7 +94,7 @@ def recommend():
     # passing all the data to the html file
     return render_template('recommend.html',title=title,poster=poster,overview=overview,vote_average=vote_average,
         vote_count=vote_count,release_date=release_date,runtime=runtime,status=status,genres=genres,
-        movie_cards=movie_cards,reviews=movie_reviews,casts=casts,cast_details=cast_details)
+        movie_cards=movie_cards,reviews=movie_reviews,casts=casts)
 
 if __name__ == '__main__':
     app.run(debug=True)
